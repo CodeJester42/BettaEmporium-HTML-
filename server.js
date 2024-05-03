c6a769c90bfa04e0803f44c762bfdb4cf4c704da
CodeJester42 <MRNEALFERNANDEZ@GMAIL.COM>
2024-05-03 12:26:13 -0500

    moved server.js and made some changes to the code, still not working properly

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3005;
+const path = require('path');
+const port = 3008;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.get('/search', (req, res) => {
     const query = req.query.query;
