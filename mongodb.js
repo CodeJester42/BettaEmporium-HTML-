1957390ec327e7055a904f090fd2cc2b6a16b1ee
CodeJester42 <MRNEALFERNANDEZ@GMAIL.COM>
2024-05-04 16:25:50 -0500

    updated uri for mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 // Connection URI
-const uri = 'mongodb+srv://mrnealfernandez:GURyHIrxPkL7c2ko@cluster0.lhjb6bt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const uri = 'mongodb+srv://mrnealfernandez:eVi6kqf58uzKrnYS@cluster0.vtkwqsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
