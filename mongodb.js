fdb86f62a97cb9c8d8dffacf1ec17d4e7233b0bf
CodeJester42 <MRNEALFERNANDEZ@GMAIL.COM>
2024-05-04 16:24:10 -0500

    changes were made on laptop but now asking me to commit from desktop?

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,7 @@
 const { MongoClient } = require('mongodb');
 
-// Connection URI
 const uri = 'mongodb+srv://mrnealfernandez:GURyHIrxPkL7c2ko@cluster0.lhjb6bt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-// Create a new MongoClient
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Connect to the MongoDB cluster
 module.exports = client;
