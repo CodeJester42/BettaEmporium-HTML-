2521022902cd5ede7fdf5db1a44268b8a2247290
CodeJester42 <MRNEALFERNANDEZ@GMAIL.COM>
2024-05-05 22:25:24 -0500

    mongoose db installed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3009;
+const port = 3004;
 const client = require('./mongodb');
 
 async function connectToDatabase() {
